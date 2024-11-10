abc305a60abdae02e78ca31840d45948811b46fd
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-10 12:22:27 +0900

    [Silver II] Title: 점프 점프, Time: 224 ms, Memory: 19920 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,27 +1,47 @@
 import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+
+    public static int N;
+    public static class Jump {
+        int index;
+        int cnt;
+        Jump(int index, int cnt) {
+            this.index = index;
+            this.cnt = cnt;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        int[] d = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-            d[i] = -1;
+        N = sc.nextInt();
+        int[] Ai = new int[N];
+        for (int i = 0; i < N; i++) {
+            Ai[i] = sc.nextInt();
         }
-        d[0] = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (d[i] == -1) continue;
-            for (int j = 1; j <= a[i]; j++) {
-                if (i + j >= n) {
-                    break;
-                }
-                if (d[i + j] == -1 || d[i + j] > d[i] + 1) {
-                    d[i + j] = d[i] + 1;
+        bfs(Ai);
+    }
+
+    public static void bfs(int[] Ai) {
+        Queue<Jump> queue = new LinkedList<>();
+        boolean[] visited = new boolean[Ai.length];
+        queue.add(new Jump(0, 0));
+        visited[0] = true;
+        while (!queue.isEmpty()) {
+            Jump now = queue.poll();
+            if (now.index == Ai.length - 1) {
+                System.out.println(now.cnt);
+                return;
+            }
+            for (int i = 0; i < Ai[now.index]; i++) {
+                if (now.index + Ai[now.index] - i < N &&!visited[now.index + Ai[now.index] - i]) {
+                    queue.add(new Jump(now.index + Ai[now.index] - i, now.cnt + 1));
+                    visited[now.index + Ai[now.index] - i] = true;
                 }
             }
         }
-        System.out.println(d[n - 1]);
+        System.out.println(-1);
     }
 }
