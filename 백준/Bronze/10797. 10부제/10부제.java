84f8fc767aaa5c80a30e9a44a43e32b6b66e262f
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-25 13:23:41 +0900

    [Bronze IV] Title: 10부제, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < 5; i++) {
+            int M = sc.nextInt();
+            if (M == N) cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
