c814081e16ce3c30db10e8813f9f9ebdc3978e7e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-26 08:34:25 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 200 ms, Memory: 18548 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a_x = sc.nextInt();
+        int a_y = sc.nextInt();
+        int a_z = sc.nextInt();
+        int c_x = sc.nextInt();
+        int c_y = sc.nextInt();
+        int c_z = sc.nextInt();
+        int b_x = c_x - a_z;
+        int b_y = c_y / a_y;
+        int b_z = c_z - a_x;
+        System.out.print(b_x + " " + b_y + " " + b_z);
+    }
+}
