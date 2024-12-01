6b78ac06217c53542f535d23c9830ee26ce5afe5
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-02 08:25:18 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 168 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Sab = sc.nextInt();
+        int Fab = sc.nextInt();
+        if (Sab <= Fab) System.out.println("high speed rail");
+        else System.out.println("flight");
+    }
+}
