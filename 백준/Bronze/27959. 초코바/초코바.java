65dda55210e9219e87a48ca3c027639f805a786e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-02 23:51:10 +0900

    [Bronze V] Title: 초코바, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        if (100 * N - M >= 0) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
