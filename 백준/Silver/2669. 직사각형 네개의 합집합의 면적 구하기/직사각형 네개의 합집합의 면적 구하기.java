e7b9d34489eecec524878fb36add8b3965ffdd70
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-05 09:12:18 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int area = 0;
         boolean[][] visited = new boolean[101][101];
-        int totalArea = 0;
         for (int i = 0; i < 4; i++) {
             int x1 = sc.nextInt();
             int y1 = sc.nextInt();
@@ -14,11 +14,11 @@ public class Main {
                 for (int k = y1; k < y2; k++) {
                     if (!visited[j][k]) {
                         visited[j][k] = true;
-                        totalArea++;
+                        area++;
                     }
                 }
             }
         }
-        System.out.println(totalArea);
+        System.out.println(area);
     }
 }
