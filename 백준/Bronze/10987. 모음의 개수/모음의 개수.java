73833a25e34416a7d6d418ad24e1fce0acb825c2
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-07 23:14:53 +0900

    [Bronze IV] Title: 모음의 개수, Time: 164 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/10987. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -3,12 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+        String word = sc.next();
         int cnt = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
-                cnt++;
-            }
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') cnt++;
         }
         System.out.println(cnt);
     }
