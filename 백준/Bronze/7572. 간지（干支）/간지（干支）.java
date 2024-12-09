c52f12b3008f9229bf5c3feffc408f7204379c93
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-09 09:06:25 +0900

    [Bronze II] Title: 간지(干支), Time: 184 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/7572. 간지（干支）/간지（干支）.java
+++ 백준/Bronze/7572. 간지（干支）/간지（干支）.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        System.out.print((char)((N % 12 + 8) % 12 + 'A'));
+        System.out.print((char)(((N % 12 + 8)) % 12 + 'A'));
         System.out.print((N % 10 + 6) % 10);
     }
 }
