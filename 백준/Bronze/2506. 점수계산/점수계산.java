2b5800408267ef3b9a5a73916b61374820ae249b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-12 18:56:53 +0900

    [Bronze III] Title: 점수계산, Time: 180 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2506. 점수계산/점수계산.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int totalScore = 0;
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] score = new int[N];
+        for (int i = 0; i < N; i++) {
+            if (i == 0) {
+                if (arr[i] == 0) score[i] = 0;
+                if (arr[i] == 1) score[i] = 1;
+            } else {
+                if (arr[i - 1] == 0 && arr[i] == 1) score[i] = 1;
+                if (arr[i - 1] == 1 && arr[i] == 1) score[i] = score[i - 1] + 1;
+                if (arr[i - 1] == 1 && arr[i] == 0) score[i] = 0;
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            totalScore += score[i];
+        }
+        System.out.println(totalScore);
+    }
+}
