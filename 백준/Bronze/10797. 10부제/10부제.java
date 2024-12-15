5aecca57fc10fcf9fd455fc9c5f6ab0e198dfcf6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-16 01:22:56 +0900

    [Bronze IV] Title: 10부제, Time: 168 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/10797. 10부제/10부제.java
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int day = sc.nextInt();
+        int[] arr = new int[5];
         int cnt = 0;
         for (int i = 0; i < 5; i++) {
-            int M = sc.nextInt();
-            if (M == N) cnt++;
+            arr[i] = sc.nextInt();
+            if (arr[i] == day) cnt++;
         }
         System.out.println(cnt);
     }
