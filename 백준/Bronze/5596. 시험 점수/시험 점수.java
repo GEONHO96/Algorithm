88942bb23b1ba5b54802d5fdc95fe34bc2517299
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-20 23:14:48 +0900

    [Bronze IV] Title: 시험 점수, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] minguk = new int[4];
+        int S = 0;
+        for (int i = 0; i < 4; i++) {
+            minguk[i] = sc.nextInt();
+            S += minguk[i];
+        }
+        int[] manse = new int[4];
+        int T = 0;
+        for (int i = 0; i < 4; i++) {
+            manse[i] = sc.nextInt();
+            T += manse[i];
+        }
+        if (S >= T) System.out.println(S);
+        else System.out.println(T);
+    }
+}
