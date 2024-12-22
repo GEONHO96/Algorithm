61dd92027e2eb0f819a3597026baccb30793c21b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-22 22:52:18 +0900

    [Bronze III] Title: 네 번째 점, Time: 188 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -5,18 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] x = new int[3];
         int[] y = new int[3];
-        int x1 = 0;
-        int y1 = 0;
         for (int i = 0; i < 3; i++) {
             x[i] = sc.nextInt();
             y[i] = sc.nextInt();
         }
-        if (x[0] == x[1] && x[1] != x[2]) x1 = x[2];
-        if (x[1] == x[2] && x[2] != x[0]) x1 = x[0];
-        if (x[2] == x[0] && x[0] != x[1]) x1 = x[1];
-        if (y[0] == y[1] && y[1] != y[2]) y1 = y[2];
-        if (y[1] == y[2] && y[2] != y[0]) y1 = y[0];
-        if (y[2] == y[0] && y[0] != y[1]) y1 = y[1];
-        System.out.print(x1 + " " + y1);
+        if (x[0] == x[1] && x[1] != x[2]) System.out.print(x[2] + " ");
+        else if (x[1] == x[2] && x[2] != x[0]) System.out.print(x[0] + " ");
+        else if (x[2] == x[0] && x[0] != x[1]) System.out.print(x[1] + " ");
+        if (y[0] == y[1] && y[1] != y[2]) System.out.print(y[2] + " ");
+        else if (y[1] == y[2] && y[2] != y[0]) System.out.print(y[0] + " ");
+        else if (y[2] == y[0] && y[0] != y[1]) System.out.print(y[1] + " ");
     }
 }
