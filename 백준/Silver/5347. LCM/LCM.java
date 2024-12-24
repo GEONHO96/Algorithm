3b150268d36e93d8a21077e007b21e38adbc1843
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-24 09:14:55 +0900

    [Silver V] Title: LCM, Time: 176 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Silver/5347. LCM/LCM.java
+++ 백준/Silver/5347. LCM/LCM.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static long GCD(long a, long b) {
+    public static long gcd(long a, long b) {
         long temp = 0;
         while (b > 0) {
             temp = a;
@@ -10,17 +10,18 @@ public class Main {
         }
         return a;
     }
-    public static long LCM(long a, long b) {
-        return (a * b) / GCD(a, b);
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,  b);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
-        for (long i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             long a = sc.nextLong();
             long b = sc.nextLong();
-            System.out.println(LCM(a, b));
+            System.out.println(lcm((int)a, (int)b));
         }
     }
 }
