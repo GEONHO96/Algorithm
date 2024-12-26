94dc69351e6dc3dac486d902fcf3f989749b60f5
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-26 23:19:43 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 164 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] x = new int[10];
-        int[] y = new int[10];
+        int sum = 0;
         int max = 0;
-        int people = 0;
         for (int i = 0; i < 10; i++) {
-            x[i] = sc.nextInt();
-            y[i] = sc.nextInt();
-            people += y[i] - x[i];
-            max = Math.max(max, people);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += (b - a);
+            if (sum > max) {
+                max = sum;
+            }
         }
         System.out.println(max);
     }
