aaf337726e3cb92a733c7b01f10bc52138e9d923
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-30 21:41:03 +0900

    [Bronze V] Title: 크냐?, Time: 192 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.java
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -6,14 +6,9 @@ public class Main {
         while (true) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            if (a == 0 && b == 0) {
-                break;
-            }
-            if (a > b) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
-            }
+            if (a == 0 && b == 0) break;
+            if (a > b) System.out.println("Yes");
+            else System.out.println("No");
         }
     }
 }
