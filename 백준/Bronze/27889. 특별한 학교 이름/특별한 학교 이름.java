b45e57b535deeaa6f1ed55ac69d3597ae902a665
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-03 07:56:13 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 164 ms, Memory: 17432 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -3,15 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        if (str.equals("NLCS")) {
-            System.out.println("North London Collegiate School");
-        } else if (str.equals("BHA")) {
-            System.out.println("Branksome Hall Asia");
-        } else if (str.equals("KIS")) {
-            System.out.println("Korea International School");
-        } else if (str.equals("SJA")) {
-            System.out.println("St. Johnsbury Academy");
-        }
+        String school = sc.next();
+        if (school.equals("NLCS")) System.out.println("North London Collegiate School");
+        if (school.equals("BHA")) System.out.println("Branksome Hall Asia");
+        if (school.equals("KIS")) System.out.println("Korea International School");
+        if (school.equals("SJA")) System.out.println("St. Johnsbury Academy");
     }
 }
