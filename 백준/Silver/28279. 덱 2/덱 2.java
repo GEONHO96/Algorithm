e798d9e51ac1d1d5835dc383ff3d28f49bfa1d45
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-14 22:05:06 +0900

    [Silver IV] Title: 덱 2, Time: 1252 ms, Memory: 252056 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+
+            if (command == 1) {
+                int value = Integer.parseInt(st.nextToken());
+                deque.addFirst(value);
+            } else if (command == 2) {
+                int value = Integer.parseInt(st.nextToken());
+                deque.addLast(value);
+            } else if (command == 3) {
+                if (deque.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(deque.pollFirst() + "\n");
+                }
+            } else if (command == 4) {
+                if (deque.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(deque.pollLast() + "\n");
+                }
+            } else if (command == 5) {
+                bw.write(deque.size() + "\n");
+            } else if (command == 6) {
+                if (deque.isEmpty()) {
+                    bw.write("1\n");
+                } else {
+                    bw.write("0\n");
+                }
+            } else if (command == 7) {
+                if (deque.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(deque.peekFirst() + "\n");
+                }
+            } else if (command == 8) {
+                if (deque.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(deque.peekLast() + "\n");
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
