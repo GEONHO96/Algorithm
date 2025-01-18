b226a9ec2041b245d0c954745dad4a11f976567d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-18 20:49:55 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int min = 2000;
+        min = Math.min(A + D, min);
+        min = Math.min(B + C, min);
+        System.out.println(min);
+    }
+}
