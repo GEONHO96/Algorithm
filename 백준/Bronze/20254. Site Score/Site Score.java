9d5ef4fdbdf932495b87e0a93c9092a67fe62cd1
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-20 22:08:08 +0900

    [Bronze V] Title: Site Score, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/20254. Site Score/Site Score.java
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int UR = sc.nextInt();
-        int TR = sc.nextInt();
-        int UO = sc.nextInt();
-        int TO = sc.nextInt();
-        int score = 56 * UR + 24 * TR + 14 * UO + 6 * TO;
-        System.out.println(score);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int UR = Integer.parseInt(st.nextToken());
+        int TR = Integer.parseInt(st.nextToken());
+        int UO = Integer.parseInt(st.nextToken());
+        int TO = Integer.parseInt(st.nextToken());
+
+        System.out.println(56 * UR + 24 * TR + 14 * UO + 6 * TO);
     }
 }
