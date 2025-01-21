ecb5e2cf6cb0ea609709280448d366aa1f0c1a1d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-21 21:52:41 +0900

    [Bronze V] Title: 時間 (Hour), Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        System.out.println(24 * X);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(24 * N);
     }
 }
