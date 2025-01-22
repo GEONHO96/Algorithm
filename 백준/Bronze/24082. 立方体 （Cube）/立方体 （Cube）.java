ae9e9f741dee16c72cbc326aef79cb184bbeb8f6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-22 22:38:52 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 176 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int X = sc.nextInt();
-        int volume = X * X * X;
-        System.out.println(volume);
+        System.out.println(X * X * X);
     }
 }
