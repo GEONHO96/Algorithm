32fd5a26c738d122a114b5c3e1c33f613749910b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-23 19:45:15 +0900

    [Bronze IV] Title: 과목선택, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/11948. 과목선택/과목선택.java
+++ 백준/Bronze/11948. 과목선택/과목선택.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int partialSum1 = 0;
-        int min = 101;
-        int partialSum2 = 0;
         int A = sc.nextInt();
         int B = sc.nextInt();
         int C = sc.nextInt();
         int D = sc.nextInt();
-        min = Math.min(A, min);
-        min = Math.min(B, min);
-        min = Math.min(C, min);
-        min = Math.min(D, min);
-        partialSum1 = A + B + C + D - min;
         int E = sc.nextInt();
         int F = sc.nextInt();
-        partialSum2 = Math.max(partialSum2, E);
-        partialSum2 = Math.max(partialSum2, F);
-        int totalScore = partialSum1 + partialSum2;
-        System.out.println(totalScore);
+
+        int[] group1 = new int[]{A, B, C, D};
+        Arrays.sort(group1);
+
+        int sum = group1[1] + group1[2] + group1[3] + Math.max(E, F);
+        System.out.println(sum);
     }
 }
