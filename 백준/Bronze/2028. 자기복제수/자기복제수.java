9cc4a81bdcb7b5adc38e70f190747afc920e3f73
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-25 22:52:11 +0900

    [Bronze II] Title: 자기복제수, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2028. 자기복제수/자기복제수.java
+++ 백준/Bronze/2028. 자기복제수/자기복제수.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] ags) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int i = 1; i <= T; i++) {
             int N = sc.nextInt();
-            int cnt = 0;
             int M = N * N;
+            int count = 0;
             while (M >= N) {
                 M /= 10;
-                cnt++;
+                count++;
             }
             int K = N * N;
-            if (K % (Math.pow(10, cnt)) == N) System.out.println("YES");
+            if (K % (Math.pow(10, count)) == N) System.out.println("YES");
             else System.out.println("NO");
-        }
+        }        
     }
 }
