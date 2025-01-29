ce3e9e8e1eed7a11166beaf3ba5c4ed611247a93
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-29 22:22:37 +0900

    [Bronze III] Title: 얼마?, Time: 728 ms, Memory: 116492 KB -BaekjoonHub

--- 백준/Bronze/9325. 얼마？/얼마？.java
+++ 백준/Bronze/9325. 얼마？/얼마？.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
             int s = sc.nextInt();
             int n = sc.nextInt();
-            int money = s;
+            int sum = 0;
             for (int j = 0; j < n; j++) {
                 int q = sc.nextInt();
                 int p = sc.nextInt();
-                money += q * p;
+                sum += q * p;
             }
-            System.out.println(money);
+            System.out.println(s + sum);
         }
     }
 }
