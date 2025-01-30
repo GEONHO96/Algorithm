abb05fda53674764e9cabe304ea9a63e64dc3473
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-30 23:09:00 +0900

    [Bronze V] Title: 세금, Time: 192 ms, Memory: 18340 KB -BaekjoonHub

--- 백준/Bronze/20492. 세금/세금.java
+++ 백준/Bronze/20492. 세금/세금.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int price1 = (N * 78) / 100;
-        int price2 = (N * 80) / 100 + (((N * 20) / 100) * 78) / 100;
-        System.out.print(price1 + " " + price2);
+        System.out.print((int)(N * 0.78) + " " + (int)(N * 0.8 + N * 0.20 * 0.78));
     }
 }
