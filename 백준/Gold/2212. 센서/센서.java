44e77d0c3a832798dabd2425995b02766d774734
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-02-10 22:06:28 +0900

    [Gold V] Title: 센서, Time: 192 ms, Memory: 17184 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -8,23 +8,23 @@ import java.util.Collections;
 public class Main {
     public static int minSensorRange(int N, int K, int[] sensors) {
         Arrays.sort(sensors);
-        
+
         ArrayList<Integer> distances = new ArrayList<>();
         for (int i = 1; i < N; i++) {
             distances.add(sensors[i] - sensors[i - 1]);
         }
-        
+
         Collections.sort(distances);
-        
+
         int result = sensors[N - 1] - sensors[0];
-        
-        for (int i = 0; i < Math.min(K - 1, N - 1); i++) {
+
+        for (int i = 0; i < Math.min(N - 1, K - 1); i++) {
             result -= distances.get(distances.size() - i - 1);
         }
-        
+
         return Math.max(0, result);
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
