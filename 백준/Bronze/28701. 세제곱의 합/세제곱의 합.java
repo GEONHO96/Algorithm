747dbb309508a5274a6abaa92e884b29155757c4
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-02-15 22:20:28 +0900

    [Bronze V] Title: 세제곱의 합, Time: 176 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
@@ -4,15 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int sum1 = (N * (N + 1)) / 2;
-        int sum2 = 0;
-        for (int i = N; i >= 1; i--) {
-            sum2 += i;
-        }
-        sum2 *= sum2;
-        int sum3 = ((N * (N + 1)) / 2) * ((N * (N + 1)) / 2);
-        System.out.println(sum1);
-        System.out.println(sum2);
-        System.out.println(sum3);
+        System.out.println((N * (N + 1)) / 2);
+        System.out.println((N * (N + 1) / 2) * (N * (N + 1) / 2));
+        System.out.println(((N * (N + 1)) / 2) * ((N * (N + 1)) / 2));
     }
 }
