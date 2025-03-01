676a3ce017a1d85012459e6fb0c530af9ddb7eff
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-01 22:33:40 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 164 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-        if (S.equals("SONGDO")) System.out.println("HIGHSCHOOL");
-        else if (S.equals("CODE")) System.out.println("MASTER");
-        else if (S.equals("2023")) System.out.println("0611");
-        else if (S.equals("ALGORITHM")) System.out.println("CONTEST");
+        String signal = sc.next();
+        if (signal.equals("SONGDO")) System.out.print("HIGHSCHOOL");
+        if (signal.equals("CODE")) System.out.print("MASTER");
+        if (signal.equals("2023")) System.out.print("0611");
+        if (signal.equals("ALGORITHM")) System.out.print("CONTEST");
     }
 }
