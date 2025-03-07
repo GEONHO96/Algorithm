ac2f35fb75a868e92187a68420e5f9a84e753d55
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-08 01:21:21 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 1.10 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,9 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] numbers) {
-        Arrays.sort(numbers);
-        int len = numbers.length;
-        return Math.max(numbers[0] * numbers[1], numbers[len - 2] * numbers[len - 1]);
+        int answer = 0;
+        return answer;
     }
 }
