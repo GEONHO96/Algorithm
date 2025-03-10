008fdfa03400667997dbddf4ca6b2110289228eb
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-10 14:20:13 +0900

    [Silver V] Title: 줄세우기, Time: 196 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int P = Integer.parseInt(br.readLine());
+        for (int i = 0; i < P; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int T = Integer.parseInt(st.nextToken());
+            int[] arr = new int[20];
+            for (int j = 0; j < 20; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+            int swapCount = 0;
+            for (int pass = 0; pass < 19; pass++) {
+                for (int j = 0; j < 19 - pass; j++) {
+                    int temp;
+                    if (arr[j] > arr[j + 1]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapCount++;
+                    }
+                }
+            }
+            bw.write(T + " " + swapCount + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
