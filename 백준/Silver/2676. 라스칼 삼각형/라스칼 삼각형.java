c9e9bd83e37bdd821ed0d4eff5f626e84e7e92fa
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-15 22:28:35 +0900

    [Silver II] Title: 라스칼 삼각형, Time: 128 ms, Memory: 15084 KB -BaekjoonHub

---
+++ 백준/Silver/2676. 라스칼 삼각형/라스칼 삼각형.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int mn = Math.min(n, n - m);
+            int result = mn * (n - mn) + 1;
+            bw.write(result + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
