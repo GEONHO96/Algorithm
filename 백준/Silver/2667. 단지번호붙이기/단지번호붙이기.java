6613a3f5b5af4d1529abd4818e602e37133fe11c
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-21 11:08:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,63 +1,70 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-
-    static int[][] apt;
+    static int N;
+    static int[][] apartment;
     static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static List<Integer> result;
-    static int N, cnt;
-
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        result = new LinkedList<>();
+        BufferedWriter bw=  new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
-        apt = new int[N][N];
+        apartment = new int[N][N];
         visited = new boolean[N][N];
-        cnt = 1;
-
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < N; j++) {
-                apt[i][j] = str.charAt(j) - '0';
+                apartment[i][j] = line.charAt(j) - '0';
             }
         }
-
+        List<Integer> complexSizes = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (apt[i][j] == 1 && !visited[i][j]) {
-                    dfs(i, j);
-                    result.add(cnt);
-                    cnt = 1;
+                if (apartment[i][j] == 1 && !visited[i][j]) {
+                    complexSizes.add(bfs(i, j));
                 }
             }
         }
-
-        Collections.sort(result);
-
-        bw.write(result.size() + "\n");
-        for (int r : result) bw.write(r + "\n");
+        Collections.sort(complexSizes);
+        bw.write(complexSizes.size() + "\n");
+        for (int size : complexSizes) {
+            bw.write(size + "\n");
+        }
         bw.flush();
         bw.close();
+        br.close();
     }
-
-    public static void dfs(int x, int y) {
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
         visited[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = dx[i] + x;
-            int ny = dy[i] + y;
-
-            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[nx][ny] && apt[nx][ny] == 1) {
-                cnt++;
-                dfs(nx, ny);
+        int count = 1;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int cx = now[0];
+            int cy = now[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
+                    if (apartment[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.offer(new int[]{nx, ny});
+                        count++;
+                    }
+                }
             }
         }
+        return count;
     }
 }
