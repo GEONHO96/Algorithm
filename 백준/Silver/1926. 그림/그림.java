76da2e6e5612f0c4b446ab157a525e058002c253
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-21 15:50:37 +0900

    [Silver I] Title: 그림, Time: 484 ms, Memory: 46152 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Main {
+    static int n, m;
+    static int[][] drawingPaper;
+    static boolean[][] visited;
+    static int numberOfDrawings, maxArea;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        drawingPaper = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                drawingPaper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        numberOfDrawings = 0;
+        maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (drawingPaper[i][j] == 1 && !visited[i][j]) {
+                    int area = bfs(i, j);
+                    maxArea = Math.max(area, maxArea);
+                    numberOfDrawings++;
+                }
+            }
+        }
+        bw.write(numberOfDrawings + "\n");
+        bw.write(maxArea + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    public static int bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+        int count = 1;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int cx = now[0];
+            int cy = now[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (drawingPaper[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.offer(new int[]{nx, ny});
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
