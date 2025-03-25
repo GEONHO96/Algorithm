16e3f72d557e31a1f3e5bad6d6fe14d2920dc2c6
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-25 19:51:19 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 120 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -1,16 +1,29 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int L = sc.nextInt();
-        int P = sc.nextInt();
-        int limitPeople = L * P;
-        int difference;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int L = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int[] arr = new int[5];
+        int[] answer = new int[5];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 5; i++) {
-            int participants = sc.nextInt();
-            difference = participants - limitPeople;
-            System.out.print(difference + " ");
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        for (int i = 0; i < 5; i++) {
+            answer[i] = arr[i] - L * P;
+            bw.write(answer[i] + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
