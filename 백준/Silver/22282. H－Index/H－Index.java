665d22c7f1490865c9879b1e19af5b0d6bd6aeaa
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-01 10:54:12 +0900

    [Silver V] Title: H-Index, Time: 380 ms, Memory: 31412 KB -BaekjoonHub

--- 백준/Silver/22282. H－Index/H－Index.java
+++ 백준/Silver/22282. H－Index/H－Index.java
@@ -1,13 +1,18 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
         int[] citations = new int[n];
         for (int i = 0; i < n; i++) {
-            citations[i] = sc.nextInt();
+            citations[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(citations);
         int count = 0;
@@ -17,6 +22,9 @@ public class Main {
                 count++;
             }
         }
-        System.out.println(count);
+        bw.write(String.valueOf(count) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
