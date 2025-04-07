167afaca7fd812462190854714f150f3a89b0046
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-07 21:30:01 +0900

    [Bronze II] Title: 더하기, Time: 168 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/10822. 더하기/더하기.java
+++ 백준/Bronze/10822. 더하기/더하기.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] S = sc.next().split(",");
-        int len = S.length;
+        String operation = sc.next();
+        String[] parts = operation.split(",");
         int sum = 0;
-        for (int i = 0; i < len; i++) {
-            sum += Integer.parseInt(S[i]);
+        for (int i = 0; i < parts.length; i++) {
+            sum += Integer.parseInt(parts[i]);
         }
         System.out.println(sum);
     }
