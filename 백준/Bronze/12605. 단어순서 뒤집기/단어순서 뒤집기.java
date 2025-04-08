b5578d1a8640e5fc3590840baf1731c1e1064385
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-08 15:47:56 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            String sentence = br.readLine();
+            String[] words = sentence.split(" ");
+            int M = words.length;
+            bw.write("Case #" + i + ": ");
+            for (int j = M - 1; j >= 0; j--) {
+                bw.write(words[j] + " ");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
