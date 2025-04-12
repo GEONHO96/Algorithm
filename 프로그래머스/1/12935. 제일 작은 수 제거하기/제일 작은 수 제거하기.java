111331c22f9efef94f73bc4684fb61a965837857
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-12 23:39:28 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 14.68 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,17 +1,6 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int[] arr) {
-        if (arr.length == 1) return new int[]{-1};
-        int min = arr[0];
-        for (int num : arr) {
-            if (num < min) min = num;
-        }
-        List<Integer> list = new ArrayList<>();
-        for (int num : arr) {
-            if (num != min) list.add(num);
-        }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = {};
+        return answer;
     }
 }
