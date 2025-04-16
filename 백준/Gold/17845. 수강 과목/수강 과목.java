7536bfbee460c11931b3ce15bf2b22e2b7fdf454
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-16 14:30:13 +0900

    [Gold V] Title: 수강 과목, Time: 156 ms, Memory: 16844 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[N + 1];
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int I = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+            for (int j = N; j >= T; j--) {
+                dp[j] = Math.max(dp[j], dp[j - T] + I);
+            }
+        }
+        bw.write(String.valueOf(dp[N]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
