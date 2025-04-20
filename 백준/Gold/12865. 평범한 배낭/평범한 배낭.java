50482ed7e1413a7ee54a49b85ae289772fd555fb
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-20 22:45:05 +0900

    [Gold V] Title: 평범한 배낭, Time: 160 ms, Memory: 18724 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int[] W = new int[N + 1];
-        int[] V = new int[N + 1];
-        int[][] dp = new int[N + 1][K + 1];
-        for (int i = 1; i <= N; i++) {
-            W[i] = sc.nextInt();
-            V[i] = sc.nextInt();
-        }
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
-                if (W[i] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], V[i] + dp[i - 1][j - W[i]]);
-                }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K + 1];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            for (int j = K; j >= W; j--) {
+                dp[j] = Math.max(dp[j], dp[j - W] + V);
             }
         }
-        System.out.println(dp[N][K]);
+        bw.write(dp[K] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
