d834d84770fbdd2ff58c5cc2e32a6eb3f01d1730
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-21 13:31:08 +0900

    [Gold V] Title: Coins, Time: 120 ms, Memory: 15084 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            int[] coin = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                coin[i]= Integer.parseInt(st.nextToken());
+            }
+            int M = Integer.parseInt(br.readLine());
+            int[] dp = new int[M + 1];
+            dp[0] = 1;
+            for (int i = 0; i < N; i++) {
+                for (int j = coin[i]; j <= M; j++) {
+                    dp[j] += dp[j - coin[i]];
+                }
+            }
+            bw.write(dp[M] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
