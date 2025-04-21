0e33619afc1ffd4e98888b2560976d439854b6d6
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-22 08:56:45 +0900

    [Silver III] Title: 피자 (Large), Time: 176 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Silver/14607. 피자 （Large）/피자 （Large）.java
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.java
@@ -1,11 +1,9 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        System.out.println((long) N * (N - 1) / 2);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        System.out.println(N * (N - 1) / 2);
     }
 }
