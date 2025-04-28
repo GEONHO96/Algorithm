42343250d9e270d7389d1a6af24658640abb8817
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-28 09:25:28 +0900

    [Silver II] Title: 편의점 2, Time: 572 ms, Memory: 43372 KB -BaekjoonHub

---
+++ 백준/Silver/14400. 편의점 2/편의점 2.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(x);
+        Arrays.sort(y);
+        int medianX = x[n / 2];
+        int medianY = y[n / 2];
+        long totalDistance = 0;
+        for (int i = 0; i < n; i++) {
+            totalDistance += Math.abs(x[i] - medianX);
+            totalDistance += Math.abs(y[i] - medianY);
+        }
+        bw.write(totalDistance + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
