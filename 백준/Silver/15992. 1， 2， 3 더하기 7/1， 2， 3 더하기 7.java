36cf510420c62c0661cb0ad5293113f0636a0968
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-29 13:36:43 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 196 ms, Memory: 21384 KB -BaekjoonHub

--- 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.java
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.java
@@ -14,7 +14,7 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         dp[0][0] = 1;
         for (int i = 1; i <= MAX_N; i++) {
-            for (int j = 1; j <= MAX_N; j++) {
+            for (int j = 1; j <+ MAX_N; j++) {
                 if (i >= 1) dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
                 if (i >= 2) dp[i][j] = (dp[i][j] + dp[i - 2][j - 1]) % MOD;
                 if (i >= 3) dp[i][j] = (dp[i][j] + dp[i - 3][j - 1]) % MOD;
