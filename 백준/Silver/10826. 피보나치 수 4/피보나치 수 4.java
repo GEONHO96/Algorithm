1ae9636ab5c285e6fbd0aab2e7e7df64171cae1e
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-30 09:34:46 +0900

    [Silver V] Title: 피보나치 수 4, Time: 152 ms, Memory: 21656 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        BigInteger[] fib = new BigInteger[n + 2];
+        fib[0] = BigInteger.ZERO;
+        fib[1] = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 2].add(fib[i - 1]);
+        }
+        bw.write(fib[n] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
