36dd514e369a10dc98eb19b5bfca56b0139b83f6
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-02 09:05:39 +0900

    [Gold V] Title: 합분해, Time: 128 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int MOD = 1_000_000_000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        long[][] dp = new long[K + 1][N + 1];
+        for (int i = 0; i <= K; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
+            }
+        }
+        bw.write(dp[K][N] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
