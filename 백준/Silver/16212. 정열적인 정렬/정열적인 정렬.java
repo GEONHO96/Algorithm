3c50c07b58e5edfb3d34fd0562e0a447d2340bb1
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-02 22:41:35 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1272 ms, Memory: 88016 KB, Score: 25 point -BaekjoonHub

--- 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] a = new int[N];
         for (int i = 0; i < N; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(a);
-        for(int i = 0; i < N; i++) {
-            System.out.print(a[i] + " ");
+        for (int i = 0; i < N; i++) {
+            bw.write(a[i] + " ");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
