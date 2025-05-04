32df5001b22423fe60b3a45c5c30670b237bf1aa
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-04 22:55:23 +0900

    [Silver V] Title: 사탕, Time: 152 ms, Memory: 15760 KB -BaekjoonHub

---
+++ 백준/Silver/11256. 사탕/사탕.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int J = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            Integer[] boxes = new Integer[N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int R = Integer.parseInt(st.nextToken());
+                int C = Integer.parseInt(st.nextToken());
+                boxes[i] = R * C;
+            }
+            Arrays.sort(boxes, Collections.reverseOrder());
+            int count = 0;
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += boxes[i];
+                count++;
+                if (sum >= J) break;
+            }
+            bw.write(count + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
