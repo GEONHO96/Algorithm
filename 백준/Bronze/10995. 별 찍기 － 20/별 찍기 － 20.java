102789ec08f1396d589128ecefcc39df0bd60c8f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-06 23:19:57 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -1,29 +1,28 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 2 * N; j++) {
-                if (i % 2 == 0) {
-                    if (j % 2 == 0) {
-                        System.out.print("*");
-                    } else if (j % 2 == 1 && j != 2 * N - 1) {
-                        System.out.print(" ");
-                    } else if (j % 2 == 1 && j == 2 * N - 1) {
-                        System.out.println();
-                    }
-                } else {
-                    if (j % 2 == 1 && j != 2 * N - 1) {
-                        System.out.print("*");
-                    } else if (j % 2 == 0) {
-                        System.out.print(" ");
-                    } else if (j % 2 == 1 && j == 2 * N - 1) {
-                        System.out.println("*");
-                    }
+            if (i % 2 == 0) {
+                for (int j = 0; j < N; j++) {
+                    bw.write("* ");
+                }
+            } else {
+                for (int j = 0; j < N; j++) {
+                    bw.write(" *");
                 }
             }
+            bw.write("\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
