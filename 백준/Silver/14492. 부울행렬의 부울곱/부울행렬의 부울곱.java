49392ac0e6d6a7d66bdf8c44de08639e22671cc3
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-18 22:48:48 +0900

    [Silver V] Title: 부울행렬의 부울곱, Time: 420 ms, Memory: 31208 KB -BaekjoonHub

---
+++ 백준/Silver/14492. 부울행렬의 부울곱/부울행렬의 부울곱.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] A = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] B = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                boolean value = false;
+                for (int k = 0; k < N; k++) {
+                    if (A[i][k] == 1 && B[k][j] == 1) {
+                        value = true;
+                        break;
+                    }
+                }
+                if (value) count++;
+            }
+        }
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
