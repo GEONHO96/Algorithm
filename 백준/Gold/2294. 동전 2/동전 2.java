9f3c7208b9ddb1ec3b81cd77dd9702a38ad19d81
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-27 14:26:25 +0900

    [Gold V] Title: 동전 2, Time: 132 ms, Memory: 15956 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, 10001);
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int i = coin; i <= k; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        bw.write((dp[k] == 10001 ? -1 : dp[k]) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
