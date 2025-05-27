c5c5a29d685117f245bbeb023417031c6c1465b5
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-27 09:41:38 +0900

    [Silver II] Title: 조합 0의 개수, Time: 104 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count2 = countFactor(n, 2) - countFactor(m, 2) - countFactor(n - m, 2);
+        int count5 = countFactor(n, 5) - countFactor(m, 5) - countFactor(n - m, 5);
+        bw.write(String.valueOf(Math.min(count2, count5)));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    static int countFactor(int n, int factor){
+        int count = 0;
+        while (n >= factor) {
+            count += n / factor;
+            n /= factor;
+        }
+        return count;
+    }
+}
