1bf2b288bd9dc59134796c4c497f9456d72b5d61
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-27 10:58:48 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 116 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int count = m * k + m;
+        bw.write(count + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
