cd45188749fc8b23535d9a43705cc1d329034498
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-28 12:21:45 +0900

    [Silver IV] Title: 수열 정렬, Time: 156 ms, Memory: 16184 KB -BaekjoonHub

---
+++ 백준/Silver/1015. 수열 정렬/수열 정렬.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    static class Pair {
+        int value, index;
+        Pair(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        Pair[] B = new Pair[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = new Pair(A[i], i);
+        }
+        Arrays.sort(B, Comparator.comparingInt(p -> p.value));
+        int[] P = new int[N];
+        for (int i = 0; i < N; i++) {
+            P[B[i].index] = i;
+        }
+        for (int i = 0; i < N; i++) {
+            bw.write(P[i] + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
