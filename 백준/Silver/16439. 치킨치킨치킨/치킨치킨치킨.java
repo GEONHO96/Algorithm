2d5940925ce5129cf608fadc8c5ae665f2624ced
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-31 21:39:41 +0900

    [Silver IV] Title: 치킨치킨치킨, Time: 128 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Silver/16439. 치킨치킨치킨/치킨치킨치킨.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] preference = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                preference[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int totalSum = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = i + 1; j < M; j++) {
+                for (int k = j + 1; k < M; k++) {
+                    int sum = 0;
+                    for (int m = 0; m < N; m++) {
+                        int best = Math.max(preference[m][i], Math.max(preference[m][j], preference[m][k]));
+                        sum += best;
+                    }
+                    totalSum = Math.max(totalSum, sum);
+                }
+            }
+        }
+        bw.write(totalSum + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
