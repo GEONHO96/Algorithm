34cf9acaa78369814761438204106622a15d471e
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-02 23:19:59 +0900

    [Silver V] Title: 연속인가? ?, Time: 116 ms, Memory: 15848 KB -BaekjoonHub

---
+++ 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        if (a * k + b == c * k + d) {
+            long fk = (long) a * k + b;
+            bw.write("Yes " + fk);
+        } else {
+            bw.write("No ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
