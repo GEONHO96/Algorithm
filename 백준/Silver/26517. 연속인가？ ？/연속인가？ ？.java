21e7307910f9fb6050e128e94aaa1fa170aa74d0
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-02 23:26:18 +0900

    [Silver V] Title: 연속인가? ?, Time: 120 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.java
+++ 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.java
@@ -6,23 +6,26 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int a, b, c, d;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int k = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
         if (a * k + b == c * k + d) {
-            long fk = (long) a * k + b;
-            bw.write("Yes " + fk);
+            bw.write("Yes " + f(k));
         } else {
             bw.write("No ");
         }
         bw.flush();
         bw.close();
         br.close();
+    }
+    public static long f(int k) {
+        return (long) a * k + b;
     }
 }
