63b31f23629774cafb0edc624add84acf6919a5d
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-06 08:30:39 +0900

    [Silver V] Title: I’ve Been Everywhere, Man, Time: 120 ms, Memory: 14916 KB -BaekjoonHub

---
+++ 백준/Silver/11645. I’ve Been Everywhere， Man/I’ve Been Everywhere， Man.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int N = Integer.parseInt(br.readLine());
+            Set<String> cities = new HashSet<>();
+            for (int i = 0; i < N; i++) {
+                String city = br.readLine();
+                cities.add(city);
+            }
+            bw.write(cities.size() + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
