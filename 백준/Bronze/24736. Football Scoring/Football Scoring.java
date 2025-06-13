48b90087b8a827fc0d8fdaf4228949deed07e059
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-13 22:36:11 +0900

    [Bronze V] Title: Football Scoring, Time: 116 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Bronze/24736. Football Scoring/Football Scoring.java
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.java
@@ -10,13 +10,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < 2; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int T = Integer.parseInt(st.nextToken());
-            int F = Integer.parseInt(st.nextToken());
-            int S = Integer.parseInt(st.nextToken());
-            int P = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            bw.write(6 * T + 3 * F + 2 * S + P + 2 * C + " ");
+             StringTokenizer st = new StringTokenizer(br.readLine());
+             int T = Integer.parseInt(st.nextToken());
+             int F = Integer.parseInt(st.nextToken());
+             int S = Integer.parseInt(st.nextToken());
+             int P = Integer.parseInt(st.nextToken());
+             int C = Integer.parseInt(st.nextToken());
+             bw.write(6 * T + 3 * F + 2 * S + P + 2 * C + " ");
         }
         bw.flush();
         bw.close();
