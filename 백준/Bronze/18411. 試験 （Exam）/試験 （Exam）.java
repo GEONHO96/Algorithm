b5b32155fa1e3079da4608997dac0cb5be61572b
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-14 23:33:42 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -1,16 +1,25 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Collections;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int sum;
-        int[] scores = new int[]{A, B, C};
-        Arrays.sort(scores);
-        sum = scores[1] + scores[2];
-        System.out.println(sum);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        Integer[] arr = new Integer[]{A, B, C};
+        Arrays.sort(arr, Collections.reverseOrder());
+        bw.write(String.valueOf(arr[0] + arr[1]));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
