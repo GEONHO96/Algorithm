4b65c1593c773b0766bb8dcdb3d9ca9c4d65ceea
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-15 21:49:12 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 108 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(X + Y + 3));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
