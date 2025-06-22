51dabe1bd7130bfa0e044603f595d1390d8f249e
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-22 22:33:57 +0900

    [Bronze V] Title: Cupcake Party, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -1,11 +1,19 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int R = sc.nextInt();
-        int S = sc.nextInt();
-        int leftover = (R * 8 + S * 3) - 28;
-        System.out.println(leftover);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int R = Integer.parseInt(br.readLine());
+        int S = Integer.parseInt(br.readLine());
+        int leftovers = R * 8 + S * 3 - 28;
+        bw.write(String.valueOf(leftovers));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
