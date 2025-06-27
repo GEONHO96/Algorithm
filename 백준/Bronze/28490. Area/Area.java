f3fd1eb852b6c5964eb1c0c05ec9d5564d715d97
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-27 22:24:42 +0900

    [Bronze IV] Title: Area, Time: 108 ms, Memory: 14248 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/28490. Area/Area.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int max = Integer.MIN_VALUE;
+        while (n-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            max = Math.max(h * w, max);
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
