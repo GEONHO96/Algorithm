1b80977cc63885beccbff51dff3ac6cc3c25af5a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-04 14:35:13 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            if (i == N - 1) {
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    bw.write("*");
+                }
+            } else {
+                for (int j = 0; j < N - i - 1; j++) {
+                    bw.write(" ");
+                }
+                bw.write("*");
+                if (i != 0) {
+                    for (int j = 0; j < 2 * i - 1; j++) {
+                        bw.write(" ");
+                    }
+                    bw.write("*");
+                }
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
