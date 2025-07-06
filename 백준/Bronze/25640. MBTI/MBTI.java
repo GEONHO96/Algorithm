562bf07b88f301acce6aa7a51fbde0cb313c7c9f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-06 16:53:36 +0900

    [Bronze IV] Title: MBTI, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/25640. MBTI/MBTI.java
+++ 백준/Bronze/25640. MBTI/MBTI.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-        int N = sc.nextInt();
-        int cnt = 0;
-        for(int i = 0; i < N; i++) {
-            String T = sc.next();
-            if (S.equals(T)) cnt++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String jinho = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (N-- > 0) {
+            String mbti = br.readLine();
+            if (mbti.equals(jinho)) count++;
         }
-        System.out.println(cnt);
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
