bc17a3d2ab3b1cd250b781d6f998fabe1dd151ed
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-07 16:37:59 +0900

    [Bronze IV] Title: !밀비 급일, Time: 128 ms, Memory: 16196 KB -BaekjoonHub

--- 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -1,17 +1,25 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         while (true) {
-            String str = sc.nextLine();
-            if (str.equals("END")) {
-                break;
+            String password = br.readLine();
+            if (password.equals("END")) break;
+            int n = password.length();
+            String S = "";
+            for (int i = n - 1; i >= 0; i--) {
+                S += password.charAt(i);
             }
-            for (int i = str.length() - 1; i >= 0; i--) {
-                System.out.print(str.charAt(i));
-            }
-            System.out.println();
+            bw.write(S + "\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
