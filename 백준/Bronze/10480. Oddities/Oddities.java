e402907bcc3e52926bbb343f6e150f4430e4eb9f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-07 16:55:06 +0900

    [Bronze IV] Title: Oddities, Time: 112 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/10480. Oddities/Oddities.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (Math.abs(x) % 2 == 0) bw.write(x + " is even" + "\n");
+            else bw.write(x + " is odd" + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
