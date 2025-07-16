8753b9f8041952da83bacd7ffae4c84faa8ca46f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-17 03:03:13 +0900

    [Bronze I] Title: 0의 개수, Time: 672 ms, Memory: 298256 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.java
+++ 백준/Bronze/11170. 0의 개수/0의 개수.java
@@ -1,21 +1,30 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            int cnt = 0;
-            StringBuilder S = new StringBuilder();
-            for (int j = N; j <= M; j++) {
-                S.append(j);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int count = 0;
+            for (int i = N; i <= M; i++) {
+                String s = String.valueOf(i);
+                for (int j = 0; j < s.length(); j++) {
+                    if (s.charAt(j) == '0') count++;
+                }
             }
-            for (int j = 0; j < S.length(); j++) {
-                if (S.charAt(j) == '0') cnt++;
-            }
-            System.out.println(cnt);
+            bw.write(count + "\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
