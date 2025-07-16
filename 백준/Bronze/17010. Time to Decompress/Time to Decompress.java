c262b1c0adf7be724c5bd794fc24de736a63114c
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-16 11:47:42 +0900

    [Bronze IV] Title: Time to Decompress, Time: 116 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/17010. Time to Decompress/Time to Decompress.java
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int L = sc.nextInt();
-        for (int i = 0; i < L; i++) {
-            int N = sc.nextInt();
-            char x = sc.next().charAt(0);
-            for (int j = 0; j < N; j++) {
-                System.out.print(x);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int L = Integer.parseInt(br.readLine());
+        while (L-- > 0) {
+            String[] split = br.readLine().split(" ");
+            int N = Integer.parseInt(split[0]);
+            char x = split[1].charAt(0);
+            for (int i = 0; i < N; i++) {
+                bw.write(x);
             }
-            System.out.println();
+            bw.write("\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
