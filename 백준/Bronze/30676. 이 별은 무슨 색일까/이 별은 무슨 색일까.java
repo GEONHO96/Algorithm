a2a3a459a8f9e6d909d5d41e5667589048a97d1f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-16 11:53:11 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.java
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.java
@@ -1,15 +1,23 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int wavelength = sc.nextInt();
-        if (620 <= wavelength && wavelength <= 780) System.out.println("Red");
-        if (590 <= wavelength && wavelength < 620) System.out.println("Orange");
-        if (570 <= wavelength && wavelength < 590) System.out.println("Yellow");
-        if (495 <= wavelength && wavelength < 570) System.out.println("Green");
-        if (450 <= wavelength && wavelength < 495) System.out.println("Blue");
-        if (425 <= wavelength && wavelength < 450) System.out.println("Indigo");
-        if (380 <= wavelength && wavelength < 425) System.out.println("Violet");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int ramda = Integer.parseInt(br.readLine());
+        if (ramda >= 620 && ramda <= 780) bw.write("Red");
+        if (ramda >= 590 && ramda < 620) bw.write("Orange");
+        if (ramda >= 570 && ramda < 590) bw.write("Yellow");
+        if (ramda >= 495 && ramda < 570) bw.write("Green");
+        if (ramda >= 450 && ramda < 495) bw.write("Blue");
+        if (ramda >= 425 && ramda < 450) bw.write("Indigo");
+        if (ramda >= 380 && ramda < 425) bw.write("Violet");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
