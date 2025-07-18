50d5c6a5e88f3717efea89211fae026817f5e28a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-18 11:06:48 +0900

    [Bronze III] Title: 경기 결과, Time: 532 ms, Memory: 206488 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5523. 경기 결과/경기 결과.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int countA = 0;
+        int countB = 0;
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if (A > B) countA++;
+            if (A < B) countB++;
+        }
+        bw.write(countA + " " + countB);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
