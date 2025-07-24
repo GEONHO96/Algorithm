0b1baa6d341d4a853235858801de08c1293e9d55
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-25 08:31:41 +0900

    [Bronze II] Title: 주차의 신, Time: 108 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Bronze/5054. 주차의 신/주차의 신.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            int min = 0;
+            for (int i = 0; i < n - 1; i++) {
+                min += arr[i + 1] - arr[i];
+            }
+            min *= 2;
+            bw.write(String.valueOf(min) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
