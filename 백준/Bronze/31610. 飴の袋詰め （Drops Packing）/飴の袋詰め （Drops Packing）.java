2180b0b0d65140656dbfa48c6d73271eae226aec
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-26 22:42:30 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 172 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -1,20 +1,12 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
-        int C = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
         int sum = A * B + C;
-        bw.write(String.valueOf(sum));
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sum);
     }
 }
