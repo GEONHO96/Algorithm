bd6f02b0369cb4b2ae4a93dc3693a4832aece102
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-27 23:10:57 +0900

    [Bronze IV] Title: 10!, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/28352. 10！/10！.java
+++ 백준/Bronze/28352. 10！/10！.java
@@ -1,22 +1,14 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        long factorial = 1;
-        for (int i = N ; i >= 1; i--) {
-            factorial *= i;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long seconds = 1;
+        for (int i = N; i >= 1; i--) {
+            seconds *= i;
         }
-        long week =  factorial / (7 * 24 * 60 * 60);
-        bw.write(String.valueOf(week));
-        bw.flush();
-        bw.close();
-        br.close();
+        long weeks = seconds / (7 * 24 * 60 * 60);
+        System.out.println(weeks);
     }
 }
