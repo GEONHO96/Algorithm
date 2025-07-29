ccdc83251dcbb2c5d7c36dd9edf4353a22b57657
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-29 13:07:40 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 168 ms, Memory: 17048 KB -BaekjoonHub

--- 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.java
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.java
@@ -1,31 +1,29 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] arr1 = new int[N];
+        int[] number = new int[N];
+        int count = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+            number[i] = Integer.parseInt(st.nextToken());
         }
-        int[] arr2 = new int[N];
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr2[i] = arr1[i];
+            arr[i] = i + 1;
         }
-        Arrays.sort(arr1);
-        int count = 0;
         for (int i = 0; i < N; i++) {
-            if (arr1[i] != arr2[i]) count++;
+            if (arr[i] == number[i]) count++;
         }
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(N - count));
         bw.flush();
         bw.close();
         br.close();
