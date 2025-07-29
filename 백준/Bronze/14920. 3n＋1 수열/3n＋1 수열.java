adf3ebb35442212e87a15b0212e16c5fa6a0ddbd
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-29 12:54:40 +0900

    [Bronze III] Title: 3n+1 수열, Time: 108 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.java
+++ 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.java
@@ -1,19 +1,24 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int C = sc.nextInt();
-        int n = 0;
-        while (C > 1) {
-            if (C % 2 == 0) {
-                C /= 2;
-                n++;
-            } else {
-                C = 3 * C + 1;
-                n++;
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int count = 1;
+        while (n > 1) {
+            if (n % 2 == 0) n /= 2;
+            else n = 3 * n + 1;
+            count++;
+            if (n == 1) break;
         }
-        System.out.println(n + 1);
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
