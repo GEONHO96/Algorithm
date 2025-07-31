b528889443c7d0776f7bb33fadc2b6e1eee249fb
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-07-31 12:29:30 +0900

    [Bronze II] Title: 거리의 합, Time: 264 ms, Memory: 16792 KB -BaekjoonHub

---
+++ 백준/Bronze/2399. 거리의 합/거리의 합.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] x = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += Math.abs(x[i] - x[j]);
+            }
+        }
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
