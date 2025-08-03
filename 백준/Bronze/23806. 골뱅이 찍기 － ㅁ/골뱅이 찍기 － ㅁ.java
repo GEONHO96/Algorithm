6c5d333d42f662e55ceae15e343349609e8a67c6
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-08-03 19:07:51 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 184 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 5 * N; j++) {
+                bw.write("@");
+            }
+            bw.write("\n");
+        }
+        for (int i = 0; i < 3 * N; i++) {
+            for (int j = 0; j < 5 * N; j++) {
+                if (j < N) {
+                    bw.write("@");
+                } else if (j >= 4 * N && j < 5 * N) {
+                    bw.write("@");
+                } else {
+                    bw.write(" ");
+                }
+            }
+            bw.write("\n");
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 5 * N; j++) {
+                bw.write("@");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
