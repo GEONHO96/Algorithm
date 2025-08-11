44eb2cb438cdfc3577d698eb1070e2e64beb2efe
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-08-11 22:05:50 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 152 ms, Memory: 15368 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < N; j++) {
+                    for (int k = 0; k < 5 * N; k++) {
+                        if (k < N || (k >= 4 * N && k < 5 * N)) {
+                            bw.write("@");
+                        } else {
+                            bw.write(" ");
+                        }
+                    }
+                    bw.write("\n");
+                }
+            } else {
+                for (int j = 0; j < N; j++) {
+                    for (int k = 0; k < 5 * N; k++) {
+                        bw.write("@");
+                    }
+                    bw.write("\n");
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
