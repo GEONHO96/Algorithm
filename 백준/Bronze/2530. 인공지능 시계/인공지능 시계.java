73b2f4ce822aa5dfc0db200a5b7450e8d13bd9fd
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-08-15 22:06:27 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 132 ms, Memory: 16312 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(br.readLine());
+        int totalTime = 3600 * A + 60 * B + C + D;
+        if (totalTime >= 86400) {
+            totalTime = totalTime % 86400;
+            bw.write(totalTime / 3600 + " " + (totalTime % 3600) / 60 + " " + totalTime % 3600 % 60);
+        } else {
+            bw.write(totalTime / 3600 + " " + (totalTime % 3600) / 60 + " " + totalTime % 3600 % 60);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
