bbbc66f5a414a6f2adc0aed9f12d332b8cce9c5b
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-08-22 13:53:19 +0900

    [Bronze IV] Title: No Brainer, Time: 96 ms, Memory: 13996 KB -BaekjoonHub

--- 백준/Bronze/4562. No Brainer/No Brainer.java
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int X = Integer.parseInt(st.nextToken());
             int Y = Integer.parseInt(st.nextToken());
