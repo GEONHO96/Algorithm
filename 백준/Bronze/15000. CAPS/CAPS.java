cc4580e27fd4bd53917864ad302b25d59cf6a748
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-08-28 23:44:10 +0900

    [Bronze V] Title: CAPS, Time: 288 ms, Memory: 23468 KB -BaekjoonHub

--- 백준/Bronze/15000. CAPS/CAPS.java
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -1,13 +1,22 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String message = sc.next();
-        StringBuilder S = new StringBuilder();
-        for (int i = 0; i < message.length(); i++) {
-            S.append(Character.toUpperCase(message.charAt(i)));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String line = br.readLine();
+        int n = line.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(Character.toUpperCase(line.charAt(i)));
         }
-        System.out.println(S);
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
