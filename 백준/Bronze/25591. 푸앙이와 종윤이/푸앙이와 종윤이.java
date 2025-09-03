bbba5e019340846ccaf6142263a3876b1a37d50a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-09-03 21:48:16 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 168 ms, Memory: 17268 KB -BaekjoonHub

--- 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
@@ -10,18 +10,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int a = 100 - n;
-        int b = 100 - m;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int a = 100 - N;
+        int b = 100 - M;
         int c = 100 - (a + b);
         int d = a * b;
         int q = d / 100;
         int r = d % 100;
         int s = c + q;
         int t = r;
-        bw.write(a + " " + b + " " + c + " " + d + " " + q + " " + r + "\n");
-        bw.write(s + " " + t);
+        bw.write(a + " " + b + " " + c + " " + d + " " + q + " " + r + "\n" + s + " " + t);
         bw.flush();
         bw.close();
     }
