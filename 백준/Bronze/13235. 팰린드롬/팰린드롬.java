73babc742fb82db542b558a5695b810c002a9a9f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-09-15 20:16:18 +0900

    [Bronze II] Title: 팰린드롬, Time: 132 ms, Memory: 16280 KB -BaekjoonHub

--- 백준/Bronze/13235. 팰린드롬/팰린드롬.java
+++ 백준/Bronze/13235. 팰린드롬/팰린드롬.java
@@ -1,14 +1,23 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-        String reverseS = "";
-        for (int i = S.length() - 1; i >= 0; i--) {
-            reverseS += S.charAt(i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String word = br.readLine();
+        int n = word.length();
+        String newWord = "";
+        for (int i = n - 1; i >= 0; i--) {
+            newWord += word.charAt(i);
         }
-        if (S.equals(reverseS)) System.out.println(true);
-        else System.out.println(false);
+        if (newWord.equals(word)) bw.write("true");
+        else bw.write("false");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
