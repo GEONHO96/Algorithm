59552ace3b02d45bd84012c3feaafd5853f463ac
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-15 22:12:25 +0900

    [Bronze III] Title: 파인만, Time: 96 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/5724. 파인만/파인만.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cnt = 0;
+        while (true) {
+            int N = Integer.parseInt(br.readLine());
+            if (N == 0) break;
+            for (int i = 1; i <= N; i++) {
+                if (i == 1) cnt = 1;
+                if (i >= 2) {
+                    cnt += i * i;
+                }
+            }
+            bw.write(String.valueOf(cnt) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
