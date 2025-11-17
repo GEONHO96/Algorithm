802b7192aa4daec2078c453be9034c9751c4561e
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-17 23:07:45 +0900

    [Bronze III] Title: 스키테일 암호, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/23080. 스키테일 암호/스키테일 암호.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int K = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < S.length(); i += K) {
+            result.append(S.charAt(i));
+        }
+        bw.write(result.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
