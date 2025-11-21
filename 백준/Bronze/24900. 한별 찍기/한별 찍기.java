0e6e3fd4cebf8df3e53f2b32519236846c54f500
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-21 23:50:56 +0900

    [Bronze V] Title: 한별 찍기, Time: 96 ms, Memory: 13996 KB -BaekjoonHub

--- 백준/Bronze/24900. 한별 찍기/한별 찍기.java
+++ 백준/Bronze/24900. 한별 찍기/한별 찍기.java
@@ -63,6 +63,6 @@ public class Main {
                 "                                          .DND=~~~~=::~~=NN.   . . 8D~~.~~~~~~=DN$ODNDNDNNNDNNNNND8+~..                 \n" +
                 "                                             8DNNI=.~~~~=NDDNNNNDDNDNN.~~~~~IDDNDND7:.                                  \n" +
                 "                                                ?DNNDD?~DD          ~NN~~=NDD$                                          \n" +
-                "                                                     :DDD.            NNNN=                                             \n");
+                "                                                     :DDD.            NNNN=                                             ");
     }
 }
