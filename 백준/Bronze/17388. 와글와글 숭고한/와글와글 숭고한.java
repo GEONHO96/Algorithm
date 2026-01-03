3401593f1ec75895b402269edbc5608edf3ab4ee
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-03 22:12:49 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int S = sc.nextInt();
-        int K = sc.nextInt();
-        int H = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
         if (S + K + H >= 100) {
-            System.out.println("OK");
-        }else {
-            if (S < K && K < H) {
-                System.out.println("Soongsil");
-            } else if (S < H && H < K) {
-                System.out.println("Soongsil");
-            } else if (K < H && H < S) {
-                System.out.println("Korea");
-            } else if (K < S && S < H) {
-                System.out.println("Korea");
-            } else if (H < S && S < K) {
-                System.out.println("Hanyang");
-            } else if (H < K && K < S) {
-                System.out.println("Hanyang");
-            }
+            bw.write("OK");
+        } else {
+            if (S < K && S < H) bw.write("Soongsil");
+            if (K < H && K < S) bw.write("Korea");
+            if (H < S && H < K) bw.write("Hanyang");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
