141b1ce826c529b538898cea1d46f7387ebbaf7b
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-03 22:57:22 +0900

    [Bronze II] Title: 카드 역배치, Time: 112 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[20];
+        int cnt = 10;
+        for (int i = 0; i < 20; i++) {
+            arr[i] = i + 1;
+        }
+        while (cnt-- > 0) {
+            int temp = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int i = a - 1; i < (a + b - 1) / 2; i++) {
+                temp = arr[i];
+                arr[i] = arr[a + b - 2 - i];
+                arr[a + b - 2 - i] = temp;
+            }
+        }
+        for (int i = 0; i < 20; i++) {
+            bw.write(String.valueOf(arr[i]) + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
