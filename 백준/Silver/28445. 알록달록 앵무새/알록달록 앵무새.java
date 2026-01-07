9048c29a6cee51bb272f5e33624596718e03ef5a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-07 15:54:15 +0900

    [Silver V] Title: 알록달록 앵무새, Time: 136 ms, Memory: 16236 KB -BaekjoonHub

--- 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.java
+++ 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.java
@@ -3,9 +3,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.HashSet;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
