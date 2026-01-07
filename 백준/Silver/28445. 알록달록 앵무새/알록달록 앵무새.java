3f63d009ae24e1ca360ed4fe50227333aa7e040f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-07 15:30:13 +0900

    [Silver V] Title: 알록달록 앵무새, Time: 132 ms, Memory: 16264 KB -BaekjoonHub

---
+++ 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] father = br.readLine().split(" ");
+        String[] mother = br.readLine().split(" ");
+        String[] parents = new String[4];
+        parents[0] = father[0];
+        parents[1] = father[1];
+        parents[2] = mother[0];
+        parents[3] = mother[1];
+        Set<String> set = new HashSet<>(Arrays.asList(parents));
+        String[] uniqueArray = set.toArray(new String[0]);
+        Arrays.sort(uniqueArray);
+        int length = uniqueArray.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                bw.write(uniqueArray[i] + " " + uniqueArray[j] + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
