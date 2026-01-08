eee539f759b857453155856f573d90533c5a9a3b
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-08 23:04:31 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 2, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/24314. 알고리즘 수업 － 점근적 표기 2/알고리즘 수업 － 점근적 표기 2.java
+++ 백준/Silver/24314. 알고리즘 수업 － 점근적 표기 2/알고리즘 수업 － 점근적 표기 2.java
@@ -14,7 +14,7 @@ public class Main {
         int a0 = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(br.readLine());
         int n0 = Integer.parseInt(br.readLine());
-        if (a1 >= c && a1 * n0 + a0 >= c * n0) bw.write("1");
+        if (c <= a1 && a1 * n0 + a0 >= c * n0) bw.write("1");
         else bw.write("0");
         bw.flush();
         bw.close();
