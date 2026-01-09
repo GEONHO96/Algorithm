966240aec106832ee29be6f6c86d8aa374dafb09
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-09 09:21:00 +0900

    [Bronze III] Title: 주식 투자, Time: 420 ms, Memory: 39844 KB -BaekjoonHub

--- 백준/Bronze/13416. 주식 투자/주식 투자.java
+++ 백준/Bronze/13416. 주식 투자/주식 투자.java
@@ -3,9 +3,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,17 +13,17 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            int maxProfit = 0;
             int N = Integer.parseInt(br.readLine());
+            int maxProfit = 0;
             for (int i = 0; i < N; i++) {
-                StringTokenizer st =  new StringTokenizer(br.readLine());
+                StringTokenizer st = new StringTokenizer(br.readLine());
                 int A = Integer.parseInt(st.nextToken());
                 int B = Integer.parseInt(st.nextToken());
                 int C = Integer.parseInt(st.nextToken());
-                Integer[] arr = new Integer[]{A, B, C};
-                Arrays.sort(arr, Collections.reverseOrder());
-                if (arr[0] < 0) continue;
-                else maxProfit += arr[0];
+                Integer[] stock = new Integer[]{A, B, C};
+                Arrays.sort(stock, Collections.reverseOrder());
+                if (stock[0] < 0) continue;
+                else maxProfit += stock[0];
             }
             bw.write(String.valueOf(maxProfit) + "\n");
         }
