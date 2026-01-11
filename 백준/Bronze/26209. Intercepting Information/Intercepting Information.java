08de9a83303c9ac153c7444690ac6b2f0cf71de4
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-11 09:08:26 +0900

    [Bronze V] Title: Intercepting Information, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/26209. Intercepting Information/Intercepting Information.java
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N1 = sc.nextInt();
-        int N2 = sc.nextInt();
-        int N3 = sc.nextInt();
-        int N4 = sc.nextInt();
-        int N5 = sc.nextInt();
-        int N6 = sc.nextInt();
-        int N7 = sc.nextInt();
-        int N8 = sc.nextInt();
-        if ((N1 == 0 || N1 == 1) && (N2 == 0 || N2 == 1) && (N3 == 0 || N3 == 1) && (N4 == 0 || N4 == 1) && (N5 == 0 || N5 == 1) && (N6 == 0 || N6 == 1) && (N7 == 0 || N7 == 1) && (N8 == 0 || N8 == 1)) System.out.println("S");
-        else System.out.println("F");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] N = new int[8];
+        int cnt = -1;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 8; i++) {
+            N[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < 8; i++) {
+            if (N[i] == 9) cnt++;
+        }
+        if (cnt >= 0) bw.write("F");
+        else bw.write("S");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
