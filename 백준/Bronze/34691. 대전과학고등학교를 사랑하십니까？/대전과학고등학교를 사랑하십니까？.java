15fe0695c4fd03512106a00682917e8ce43bde75
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-13 23:51:53 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 232 ms, Memory: 18892 KB -BaekjoonHub

--- 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.java
+++ 백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.java
@@ -1,22 +1,14 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         while (true) {
-            String str = br.readLine();
+            String str = sc.next();
             if (str.equals("end")) break;
-            else if (str.equals("animal")) bw.write("Panthera tigris\n");
-            else if (str.equals("flower")) bw.write("Forsythia koreana\n");
-            else if (str.equals("tree")) bw.write("Pinus densiflora\n");
+            else if (str.equals("animal")) System.out.println("Panthera tigris");
+            else if (str.equals("flower")) System.out.println("Forsythia koreana");
+            else if (str.equals("tree")) System.out.println("Pinus densiflora");
         }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
