a3899870fc2635849e963656423a69189148dc02
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-15 01:01:57 +0900

    [Silver V] Title: 카우버거, Time: 140 ms, Memory: 16400 KB -BaekjoonHub

--- 백준/Silver/15720. 카우버거/카우버거.java
+++ 백준/Silver/15720. 카우버거/카우버거.java
@@ -3,9 +3,9 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,29 +15,44 @@ public class Main {
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
         int D = Integer.parseInt(st.nextToken());
+        int min = 0;
+        min = Math.min(B, Math.min(C, D));
+        int originalPrice = 0;
+        int discountPrice = 0;
         Integer[] burger = new Integer[B];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < B; i++) {
+            burger[i] = Integer.parseInt(st.nextToken());
+            originalPrice += burger[i];
+        }
+        Arrays.sort(burger, Collections.reverseOrder());
         Integer[] side = new Integer[C];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            side[i] = Integer.parseInt(st.nextToken());
+            originalPrice += side[i];
+        }
+        Arrays.sort(side, Collections.reverseOrder());
         Integer[] beverage = new Integer[D];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < B; i++) burger[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(burger, Collections.reverseOrder());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < C; i++) side[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(side, Collections.reverseOrder());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < D; i++) beverage[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < D; i++) {
+            beverage[i] = Integer.parseInt(st.nextToken());
+            originalPrice += beverage[i];
+        }
         Arrays.sort(beverage, Collections.reverseOrder());
-        int originalPrice = 0;
-        for (int i = 0; i < B; i++) originalPrice += burger[i];
-        for (int i = 0; i < C; i++) originalPrice += side[i];
-        for (int i = 0; i < D; i++) originalPrice += beverage[i];
-        int discountPrice = 0;
-        int min = Math.min(B, Math.min(C, D));
-        for (int i = 0; i < min; i++) discountPrice += (int) ((burger[i] + side[i] + beverage[i]) * 0.9);
-        for (int i = min; i < B; i++) discountPrice += burger[i];
-        for (int i = min; i < C; i++) discountPrice += side[i];
-        for (int i = min; i < D; i++) discountPrice += beverage[i];
-        bw.write(originalPrice + "\n" + discountPrice);
+        for (int i = 0; i < min; i++) {
+            discountPrice += 0.9 * (burger[i] + side[i] + beverage[i]);
+        }
+        for (int i = min; i < B; i++) {
+            discountPrice += burger[i];
+        }
+        for (int i = min; i < C; i++) {
+            discountPrice += side[i];
+        }
+        for (int i = min; i < D; i++) {
+            discountPrice += beverage[i];
+        }
+        bw.write(String.valueOf(originalPrice) + "\n" + String.valueOf(discountPrice));
         bw.flush();
         bw.close();
         br.close();
