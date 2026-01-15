de5b2ebe66e3adaad6b0a2bcb12f459866ecb6fc
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-15 19:59:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,19 +1,31 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int count = 0;
-        while (N >= 0) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (N > 2) {
             if (N % 5 == 0) {
-                count += N / 5;
-                System.out.println(count);      
-                return;
+                cnt += N / 5;
+                N %= 5;
+                break;
             }
             N -= 3;
-            count++;    
+            cnt++;
         }
-        System.out.println(-1);
+        if (N <= 2 && N > 0) {
+            bw.write("-1");
+        } else if (N == 0) {
+            bw.write(String.valueOf(cnt));
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
