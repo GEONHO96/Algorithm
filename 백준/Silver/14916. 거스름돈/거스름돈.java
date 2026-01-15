866cebfeffed536ea6e39a6555d830523b9e2c1a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-15 23:59:07 +0900

    [Silver V] Title: 거스름돈, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,19 +1,31 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count = 0;
-        while (n >= 0) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (n > 1) {
             if (n % 5 == 0) {
-                count += n / 5;
-                System.out.println(count);
-                return;
+                cnt += n / 5;
+                n %= 5;
             }
+            if (n <= 1) break;
             n -= 2;
-            count++;
+            cnt++;
         }
-        System.out.println(-1);
+        if (n == 1) {
+            bw.write(String.valueOf("-1"));
+        } else {
+            bw.write(String.valueOf(cnt));
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
