93724293f548d8d07654fef27159853d0d82af91
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-18 11:18:44 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 752 ms, Memory: 51240 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,33 +1,44 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Coordinate implements Comparable<Coordinate> {
     int x;
     int y;
-    public Coordinate(int x, int y) {
+    Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
     @Override
     public int compareTo(Coordinate coordinate) {
-        if (this.y == coordinate.y)
+        if (this.y == coordinate.y) {
             return this.x - coordinate.x;
+        }
         return this.y - coordinate.y;
     }
 }
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         Coordinate[] coordinate = new Coordinate[N];
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             coordinate[i] = new Coordinate(x, y);
         }
         Arrays.sort(coordinate);
-        for (int i = 0; i < N; i++)
-            System.out.println(coordinate[i].x + " " + coordinate[i].y);
+        for (int i = 0; i < N; i++) {
+            bw.write(coordinate[i].x + " " + coordinate[i].y + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
