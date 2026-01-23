b4cf8a8e6ba5604e0a5750b5cbfe7c2d9f2ba5e1
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-23 18:51:04 +0900

    [Bronze II] Title: 헤일스톤 수열, Time: 428 ms, Memory: 27748 KB -BaekjoonHub

---
+++ 백준/Bronze/3943. 헤일스톤 수열/헤일스톤 수열.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int max = 0;
+            if (n == 1) bw.write("1\n");
+            else {
+                while (n > 1) {
+                    if (n % 2 == 0) {
+                        max = Math.max(max, n);
+                        n /= 2;
+                    } else {
+                        max = Math.max(max, n);
+                        n = 3 * n + 1;
+                    }
+                }
+                bw.write(max + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
