75a2a12eda71698c805d94e427c3f2a7dd438129
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-25 22:47:54 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1268 ms, Memory: 88192 KB, Score: 25 point -BaekjoonHub

--- 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -3,8 +3,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
