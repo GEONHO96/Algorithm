d6c71eefe81821068da21a63de454f6bae649b3c
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-26 23:45:36 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 164 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.java
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.java
@@ -1,20 +1,12 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int R = Integer.parseInt(br.readLine());
-        int G = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int G = sc.nextInt();
+        int B = sc.nextInt();
         int sum = R * 3 + G * 4 + B * 5;
-        bw.write(String.valueOf(sum));
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sum);
     }
 }
