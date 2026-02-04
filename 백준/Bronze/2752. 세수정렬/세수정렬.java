931cc59a9978af5fc1768a6d4c3b3054eb23f07e
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-04 22:42:04 +0900

    [Bronze IV] Title: 세수정렬, Time: 108 ms, Memory: 16284 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -1,16 +1,26 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int[] arr = new int[3];
-        for(int i = 0; i < 3; i++) {
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
         for (int i = 0; i < 3; i++) {
-            System.out.print(arr[i] + " ");
+            bw.write(arr[i] + " ");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
