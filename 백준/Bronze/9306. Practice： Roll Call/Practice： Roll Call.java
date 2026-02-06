5591e629bbe916b6119c7963afe6e7e48aee2228
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-06 23:50:18 +0900

    [Bronze IV] Title: Practice: Roll Call, Time: 144 ms, Memory: 16852 KB -BaekjoonHub

--- 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.java
+++ 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.java
@@ -3,7 +3,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
