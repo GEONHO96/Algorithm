c1bffcc2f6d53224cfb24631ef36498aa5de8b0d
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-15 23:33:52 +0900

    [Silver IV] Title: Final Score, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/15233. Final Score/Final Score.java
+++ 백준/Silver/15233. Final Score/Final Score.java
@@ -3,8 +3,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
 import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
