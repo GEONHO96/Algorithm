264a63e7cbf800c8d84ec117b2f3ee9fc7d8e05a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-18 23:27:28 +0900

    [Bronze V] Title: 13 Floors, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/34412. 13 Floors/13 Floors.java
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        if (x <= 12) System.out.print(x);
-        else System.out.print(x + 1);
+        System.out.print(x <= 12 ? x : x + 1);
     }
 }
