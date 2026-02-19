c6ad71108c4c17d5be6abb728bd9353e46cb497a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-19 23:34:35 +0900

    [Bronze V] Title: Multiply, Time: 556 ms, Memory: 55680 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,13 +1,24 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        BigInteger A = sc.nextBigInteger();
-        BigInteger B = sc.nextBigInteger();
-        System.out.println(A.multiply(B));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
+        BigInteger C = A.multiply(B);
+        bw.write(String.valueOf(C));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
