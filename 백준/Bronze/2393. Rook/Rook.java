3929da4e020651706f8b3b456e8f2f7671a46f69
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-19 23:36:22 +0900

    [Bronze V] Title: Rook, Time: 84 ms, Memory: 13968 KB -BaekjoonHub

--- 백준/Bronze/2393. Rook/Rook.java
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("  ___  ___  ___\n" +
+        System.out.print("  ___  ___  ___\n" +
                 "  | |__| |__| |\n" +
                 "  |           |\n" +
                 "   \\_________/\n" +
