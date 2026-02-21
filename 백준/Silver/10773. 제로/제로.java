48827a0c9ae73fc12ad76dbea4486b2cfb9c85c3
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-21 18:09:33 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23996 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,25 +1,31 @@
-import java.util.Stack;
-import java.util.StringTokenizer;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int K = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < K; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            if (N != 0) stack.push(N);
-            if (N == 0) stack.pop();
-        }
         int sum = 0;
-        while (!stack.isEmpty()) {
-            sum += stack.peek();
-            stack.pop();
+        Stack<Integer> stack = new Stack<>();
+        while (K-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
         }
-        System.out.println(sum);
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
