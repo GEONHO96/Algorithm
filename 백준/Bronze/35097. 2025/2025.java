ccda49254e8ca97219f5bf7cb58de0668bc2fc2d
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-25 23:39:13 +0900

    [Bronze IV] Title: 2025, Time: 124 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Bronze/35097. 2025/2025.java
+++ 백준/Bronze/35097. 2025/2025.java
@@ -1,20 +1,26 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         while (true) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(br.readLine());
             int sum = 0;
             if (n == 0) break;
-            else {
-                for (int i = 1; i <= n; i++) {
-                    for (int j = 1; j <= n; j++) {
-                        sum += i * j;
-                    }
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    sum += i * j;
                 }
-                System.out.println(sum);
             }
+            bw.write(sum + "\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
