82a9c3003438d2dacff5b6eef1cf0b572d927a68
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-01 16:47:56 +0900

    [Bronze II] Title: 연속구간, Time: 112 ms, Memory: 15892 KB -BaekjoonHub

---
+++ 백준/Bronze/2495. 연속구간/연속구간.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < 3; i++) {
+            int max = 0;
+            int num = Integer.parseInt(br.readLine());
+            String S = String.valueOf(num);
+            int length = S.length();
+            int len = 1;
+            for (int j = 0; j < length - 1; j++) {
+                if (S.charAt(j) == S.charAt(j + 1)) len++;
+                else len = 1;
+                max = Math.max(max, len);
+            }
+            bw.write(max + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
