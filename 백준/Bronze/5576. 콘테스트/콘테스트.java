b19311d0bf2cb41e7a39a46ecf5f0a7005423bca
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-02 23:22:48 +0900

    [Bronze II] Title: 콘테스트, Time: 120 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Bronze/5576. 콘테스트/콘테스트.java
+++ 백준/Bronze/5576. 콘테스트/콘테스트.java
@@ -4,24 +4,25 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] W = new int[10];
+        Integer[] W = new Integer[10];
+        Integer[] K = new Integer[10];
         for (int i = 0; i < 10; i++) {
             W[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(W);
-        int[] K = new int[10];
+        Arrays.sort(W, Collections.reverseOrder());
         for (int i = 0; i < 10; i++) {
             K[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(K);
-        int sumW = W[7] + W[8] + W[9];
-        int sumK = K[7] + K[8] + K[9];
-        bw.write(sumW + " " + sumK);
+        Arrays.sort(K, Collections.reverseOrder());
+        int top3W = W[0] + W[1] + W[2];
+        int top3K = K[0] + K[1] + K[2];
+        bw.write(top3W + " " + top3K);
         bw.flush();
         bw.close();
         br.close();
