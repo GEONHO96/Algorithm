6a340ba54c00d49fe85d171710cc1b6e9b82e96f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-02 22:07:54 +0900

    [Bronze II] Title: 음계, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,23 +1,36 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] array = new int[8];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Integer[] arr = new Integer[8];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 8; i++) {
-            array[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int[] sequence1 = new int[8];
+        Integer[] ascArr = new Integer[8];
         for (int i = 0; i < 8; i++) {
-            sequence1[i] = i + 1;
+            ascArr[i] = arr[i];
         }
-        int[] sequence2 = new int[8];
+        Arrays.sort(ascArr);
+        Integer[] descArr = new Integer[8];
         for (int i = 0; i < 8; i++) {
-            sequence2[i] = 8 - i;
+            descArr[i] = arr[i];
         }
-        if (Arrays.equals(array, sequence1)) System.out.println("ascending");
-        else if (Arrays.equals(array, sequence2)) System.out.println("descending");
-        else System.out.println("mixed");
+        Arrays.sort(descArr, Collections.reverseOrder());
+        if (Arrays.equals(arr, ascArr)) bw.write("ascending");
+        else if (Arrays.equals(arr, descArr)) bw.write("descending");
+        else bw.write("mixed");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
