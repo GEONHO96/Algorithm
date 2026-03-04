282c77be184ed757a95df304301173dc70d00f8f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-04 23:50:09 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 172 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int V = sc.nextInt();
+        System.out.print(T * V);
+    }
+}
