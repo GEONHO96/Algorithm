470dc54b7c19048bc89108bb441389c9b282b961
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-11 22:33:01 +0900

    [Bronze IV] Title: 모음의 개수, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10987. 모음의 개수/모음의 개수.java
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -1,13 +1,22 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String word = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String S = br.readLine();
+        int length = S.length();
         int cnt = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') cnt++;
+        for (int i = 0; i < length; i++) {
+            if (S.charAt(i) == 'a' || S.charAt(i) == 'e' || S.charAt(i) == 'i' || S.charAt(i) == 'o' || S.charAt(i) == 'u') cnt++;
         }
-        System.out.println(cnt);
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
