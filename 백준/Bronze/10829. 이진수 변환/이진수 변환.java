c0c68e92d0fb8be440299a45992eeb3fd3e18b2c
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-12 23:13:06 +0900

    [Bronze II] Title: 이진수 변환, Time: 108 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/10829. 이진수 변환/이진수 변환.java
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.java
@@ -9,8 +9,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         long N = Long.parseLong(br.readLine());
-        String LongBinary = Long.toBinaryString(N);
-        bw.write(LongBinary);
+        String longBinary = Long.toBinaryString(N);
+        bw.write(longBinary);
         bw.flush();
         bw.close();
         br.close();
