6ae67f5791dc978b3a243aa85ad37d944ac74a4d
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-15 18:14:30 +0900

    [Bronze II] Title: 유학 금지, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String word = br.readLine();
+        int length = word.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            if (word.charAt(i) == 'C' || word.charAt(i) == 'A' || word.charAt(i) == 'M' || word.charAt(i) == 'B' || word.charAt(i) == 'R' || word.charAt(i) == 'I' || word.charAt(i) == 'D' || word.charAt(i) == 'G' || word.charAt(i) == 'E') {
+                continue;
+            } else {
+                sb.append(word.charAt(i));
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
