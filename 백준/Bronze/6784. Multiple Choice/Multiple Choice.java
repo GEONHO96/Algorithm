156b7e91cde0095aed625a10699a7eb56b3baa70
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-28 13:34:33 +0900

    [Bronze IV] Title: Multiple Choice, Time: 120 ms, Memory: 15004 KB -BaekjoonHub

---
+++ 백준/Bronze/6784. Multiple Choice/Multiple Choice.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        String[] A = new String[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = br.readLine();
+        }
+        String[] B = new String[N];
+        for (int i = 0; i < N; i++) {
+            B[i] = br.readLine();
+        }
+        for (int i = 0; i < N; i++) {
+            if (A[i].equals(B[i])) cnt++;
+        }
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
