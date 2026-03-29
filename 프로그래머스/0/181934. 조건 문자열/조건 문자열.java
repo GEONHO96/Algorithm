7a7a2f391b5890314aa058eed4fc46d5bf2c3e76
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-29 19:48:10 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -1,23 +1,29 @@
 class Solution {
     public int solution(String ineq, String eq, int n, int m) {
-        int answer = 0;
-        if (ineq.equals("<") && eq.equals("=")) {
-            if (n <= m) {
-                answer = 1;
-            }
-        } else if (ineq.equals("<") && !eq.equals("=")) {
-            if (n < m) {
-                answer = 1;
-            }
-        } else if (ineq.equals(">") && eq.equals("=")) {
+        if (ineq.equals(">") && eq.equals("=")) {
             if (n >= m) {
-                answer = 1;
+                return 1;
+            } else {
+                return 0;
             }
-        } else if (ineq.equals(">") && !eq.equals("=")) {
+        } else if (ineq.equals("<") && eq.equals("=")) {
+            if (n <= m) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (ineq.equals(">") && eq.equals("!")) {
             if (n > m) {
-                answer = 1;
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (n < m) {
+                return 1;
+            } else {
+                return 0;
             }
         }
-        return answer;
     }
 }
