eedcc2d8d05dd22155307adc374127c5a196928b
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-29 16:45:10 +0900

    [Bronze IV] Title: Goodbye, Code Jam, Time: 116 ms, Memory: 15808 KB -BaekjoonHub

--- 백준/Bronze/29738. Goodbye， Code Jam/Goodbye， Code Jam.java
+++ 백준/Bronze/29738. Goodbye， Code Jam/Goodbye， Code Jam.java
@@ -11,10 +11,10 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         for (int i = 1; i <= T; i++) {
             int N = Integer.parseInt(br.readLine());
-            if (N > 4500) bw.write("Case #" + i + ": " + "Round 1\n");
-            else if (N > 1000) bw.write("Case #" + i + ": " + "Round 2\n");
-            else if (N > 25) bw.write("Case #" + i + ": " + "Round 3\n");
-            else bw.write("Case #" + i + ": " + "World Finals\n");
+            if (N > 4500) bw.write("Case #" + i + ": Round 1\n");
+            else if (N > 1000) bw.write("Case #" + i + ": Round 2\n");
+            else if (N > 25) bw.write("Case #" + i + ": Round 3\n");
+            else bw.write("Case #" + i + ": World Finals\n");
         }
         bw.flush();
         bw.close();
