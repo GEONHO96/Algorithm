f2f9e1db136cffe2d313e99ba2516bec790ab81a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-03-29 19:36:39 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,9 +1,12 @@
+import java.util.HashSet;
+
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num - start_num +1 ];
-        for (int i = 0; i < end_num - start_num + 1; i++) {
-            answer[i] = i + start_num;
+        int length = end_num - start_num + 1;
+        int[] answer = new int[length];
+        for (int i = 0; i < length; i++) {
+            answer[i] = start_num + i;
         }
         return answer;
-    }
+    }    
 }
