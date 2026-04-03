915bf0104053f9359a18fef8ae00405747b6c51a
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-04-03 23:39:23 +0900

    [Bronze IV] Title: Hard choice, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/15059. Hard choice/Hard choice.java
+++ 백준/Bronze/15059. Hard choice/Hard choice.java
@@ -1,18 +1,29 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int Ca = sc.nextInt();
-        int Ba = sc.nextInt();
-        int Pa = sc.nextInt();
-        int Cr = sc.nextInt();
-        int Br = sc.nextInt();
-        int Pr = sc.nextInt();
-        int sum = 0;
-        if (Cr >= Ca) sum += Cr - Ca;
-        if (Br >= Ba) sum += Br - Ba;
-        if (Pr >= Pa) sum += Pr - Pa;
-        System.out.println(sum);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int Ca = Integer.parseInt(st.nextToken());
+        int Ba = Integer.parseInt(st.nextToken());
+        int Pa = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int Cr = Integer.parseInt(st.nextToken());
+        int Br = Integer.parseInt(st.nextToken());
+        int Pr = Integer.parseInt(st.nextToken());
+        int passengers = 0;
+        if (Ca <= Cr) passengers += Cr - Ca;
+        if (Ba <= Br) passengers += Br - Ba;
+        if (Pa <= Pr) passengers += Pr - Pa;
+        bw.write(String.valueOf(passengers));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
