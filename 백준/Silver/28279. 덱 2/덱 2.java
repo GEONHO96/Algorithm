906e1a5215a06b30729cd9610ca1518d557ec82f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-04-05 00:46:19 +0900

    [Silver IV] Title: 덱 2, Time: 1516 ms, Memory: 345392 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -3,58 +3,53 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int command = Integer.parseInt(st.nextToken());
-            if (command == 1) {
-                int value = Integer.parseInt(st.nextToken());
-                deque.addFirst(value);
-            } else if (command == 2) {
-                int value = Integer.parseInt(st.nextToken());
-                deque.addLast(value);
-            } else if (command == 3) {
+        LinkedList<Integer> deque = new LinkedList<>();
+        while (N-- > 0) {
+            String[] command = br.readLine().split(" ");
+            if (Integer.parseInt(command[0]) == 1) {
+                int X = Integer.parseInt(command[1]);
+                deque.addFirst(X);
+            } else if (Integer.parseInt(command[0]) == 2) {
+                int X = Integer.parseInt(command[1]);
+                deque.addLast(X);
+            } else if (Integer.parseInt(command[0]) == 3) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write(deque.pollFirst() + "\n");
+                    bw.write(deque.removeFirst() + "\n");
                 }
-            } else if (command == 4) {
+            } else if (Integer.parseInt(command[0]) == 4) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write(deque.pollLast() + "\n");
+                    bw.write(deque.removeLast() + "\n");
                 }
-            } else if (command == 5) {
+            } else if (Integer.parseInt(command[0]) == 5) {
                 bw.write(deque.size() + "\n");
-            } else if (command == 6) {
+            } else if (Integer.parseInt(command[0]) == 6) {
                 if (deque.isEmpty()) {
-                    bw.write("1\n");
+                    bw.write("1" + "\n");
                 } else {
-                    bw.write("0\n");
+                    bw.write("0" + "\n");
                 }
-            } else if (command == 7) {
+            } else if (Integer.parseInt(command[0]) == 7) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write(deque.peekFirst() + "\n");
+                    bw.write(deque.getFirst() + "\n");
                 }
-            } else if (command == 8) {
+            } else if (Integer.parseInt(command[0]) == 8) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write(deque.peekLast() + "\n");
+                    bw.write(deque.getLast() + "\n");
                 }
             }
         }
