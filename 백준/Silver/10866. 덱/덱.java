9058cf6e018ce60de6cd0b2698caeec223e87b92
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-04-05 00:34:09 +0900

    [Silver IV] Title: 덱, Time: 204 ms, Memory: 20200 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -3,59 +3,53 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Deque;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-
-            if (command.equals("push_front")) {
-                int value = Integer.parseInt(st.nextToken());
-                deque.addFirst(value);
-            } else if (command.equals("push_back")) {
-                int value = Integer.parseInt(st.nextToken());
-                deque.addLast(value);
-            } else if (command.equals("pop_front")) {
+        LinkedList<Integer> deque = new LinkedList<>();
+        while (N-- > 0) {
+            String[] command = br.readLine().split(" ");
+            if (command[0].equals("push_front")) {
+                int X = Integer.parseInt(command[1]);
+                deque.addFirst(X);
+            } else if (command[0].equals("push_back")) {
+                int X = Integer.parseInt(command[1]);
+                deque.addLast(X);
+            } else if (command[0].equals("pop_front")) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write(deque.pollFirst() + "\n");
+                    bw.write(deque.removeFirst() + "\n");
                 }
-            } else if (command.equals("pop_back")) {
-                if (deque.isEmpty()) {
-                    bw.write("-1\n");
-                } else {
-                    bw.write(deque.pollLast() + "\n");
-                }
-            } else if (command.equals("size")) {
+            } else if (command[0].equals("size")) {
                 bw.write(deque.size() + "\n");
-            } else if (command.equals("empty")) {
+            } else if (command[0].equals("pop_back")) {
                 if (deque.isEmpty()) {
-                    bw.write("1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write("0\n");
+                    bw.write(deque.removeLast() + "\n");
                 }
-            } else if (command.equals("front")) {
+            } else if (command[0].equals("empty")) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("1" + "\n");
                 } else {
-                    bw.write(deque.peekFirst() + "\n");
+                    bw.write("0" + "\n");
                 }
-            } else if (command.equals("back")) {
+            } else if (command[0].equals("front")) {
                 if (deque.isEmpty()) {
-                    bw.write("-1\n");
+                    bw.write("-1" + "\n");
                 } else {
-                    bw.write(deque.peekLast() + "\n");
+                    bw.write(deque.getFirst() + "\n");
+                }
+            } else if (command[0].equals("back")) {
+                if (deque.isEmpty()) {
+                    bw.write("-1" + "\n");
+                } else {
+                    bw.write(deque.getLast() + "\n");
                 }
             }
         }
