7e6d56cd27940312f39f8c58355a15f4f053033c
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-04-06 23:30:50 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 104 ms, Memory: 14092 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int amount = (C - A) + (C - B);
+        bw.write(String.valueOf(amount));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
