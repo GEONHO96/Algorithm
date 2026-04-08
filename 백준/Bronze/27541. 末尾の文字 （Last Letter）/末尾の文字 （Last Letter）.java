881cdf1fd3d78e33a74a60d4bdb3bc29e6ee98e1
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-04-08 23:51:52 +0900

    [Bronze IV] Title: 末尾の文字 (Last Letter), Time: 108 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/27541. 末尾の文字 （Last Letter）/末尾の文字 （Last Letter）.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        if (S.charAt(N - 1) == 'G') bw.write(S.substring(0, N - 1));
+        else bw.write(S + "G");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
